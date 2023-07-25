5884b0a97cf36da4c175140a312489c801ee5548
biyoonx <135206920+biyoonx@users.noreply.github.com>
2023-07-25 10:45:51 +0900

    Add files via upload

---
+++ PhonePlace/src/model/NotePen.java
@@ -0,0 +1,7 @@
+package model;
+
+public interface NotePen {
+	boolean PEN_BUTTON = true;
+	
+	public boolean bluetoothPen();
+}
---
+++ PhonePlace/src/run/Run.java
@@ -0,0 +1,12 @@
+package run;
+
+import view.PhoneView;
+
+public class Run {
+
+	public static void main(String[] args) {
+		PhoneView phoneView = new PhoneView();
+		phoneView.mainMenu();
+	}
+
+}
